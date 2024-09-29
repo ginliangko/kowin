1c88c566c3fbc78bce60cfdb24b2957b653f0931
ginliangko <gl_ko@hotmail.com>
2024-09-29 21:49:16 +0800

    Renew 42 trap water

--- CodeTraining/Leetcode/C/42. Trapping Rain Water.c
+++ CodeTraining/Leetcode/C/42. Trapping Rain Water.c
@@ -15,48 +15,24 @@ The above elevation map is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In th
 */
 
 int trap(int* height, int heightSize) {
-    int w = 0;
-    int i, j, h, k;
-    
-#if 0
-    i = 0;
-again:
-    while (i + 1 < heightSize && height[i + 1] >= height[i]) { // find start point i
-        i ++;
-    }
-    j = i + 1;
-    if (j == heightSize) return 0;
-    while (j < heightSize && height[j] < height[i]) {  // find end point j
-        j ++;
-    }
-    if (j == heightSize) {
-        height[i] -= 1;  // cut the bar and retry
-        goto again;
-    }
-    if (j < heightSize) {
-        for (k = i + 1; k < j; k ++) {
-            w += height[i] - height[k];
-        }
-        w += trap(&height[j], heightSize - j);
-    }
-#else
-    i = 0;
-    j = heightSize - 1;
-    k = 0;
-    while (i <= j) {
-        if (height[i] <= height[j]) {
-            h = height[i ++];
-        } else {
-            h = height[j --];
-        }
-        if (k > h) {
-            w += k - h;
-        } else {
-            k = h;
-        }
-    }
-#endif
-    return w;
+    int front=0, end=heightSize-1;
+    int h=0, k=0, sum=0;
+
+    while(front<end) {
+        if(height[front]<=height[end]) {
+            h=height[front++];
+        } else {
+            h=height[end--];
+        }
+
+        if(k>h) {
+            sum+= k-h;
+        } else {
+            k=h;
+        }
+    }
+
+    return sum;
 }
 
 
