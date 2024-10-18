df388f1ecca4900ad74a9ca00c7d8db22a2a4f79
Gl Ko <gl_ko@hotmail.com>
2024-10-18 23:38:21 +0800

    Update 20. Valid Parentheses.c

--- CodeTraining/Leetcode/C/20. Valid Parentheses.c
+++ CodeTraining/Leetcode/C/20. Valid Parentheses.c
@@ -6,6 +6,28 @@ Given a string containing just the characters '(', ')', '{', '}', '[' and ']', d
 The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
 */
 
+// Runtime 0ms Beats 100.00%, Memory 7.78MB Beats 77.77%
+bool isValid(char* s) {
+    int len = strlen(s);
+    char *stack=malloc(len);
+    int p=0;
+
+    for(int i=0; i<len; i++) {
+        char c=s[i];
+        switch(c) {
+        case '(':
+        case '[':
+        case '{':   stack[p++]=c;    break;
+        case ')':   if(p<=0 || stack[--p]!='(')    return false;    break;
+        case ']':   if(p<=0 || stack[--p]!='[')    return false;    break;
+        case '}':   if(p<=0 || stack[--p]!='{')    return false;    break;
+        }
+    }
+    if(p>0) return false;
+    return true;
+}
+
+/* Original solution
 bool isValid(char* s) {
     int l;
     char *stack, c;
@@ -40,7 +62,7 @@ bool isValid(char* s) {
     free(stack);
     return sp == 0 ? true : false;
 }
-
+*/
 
 /*
 Difficulty:Easy
