a8033f5439b0efeeae84e4b79555804da77c408b
Gl Ko <gl_ko@hotmail.com>
2024-10-20 13:52:51 +0800

    Update 101. Symmetric Tree.c

--- CodeTraining/Leetcode/C/101. Symmetric Tree.c
+++ CodeTraining/Leetcode/C/101. Symmetric Tree.c
@@ -38,40 +38,49 @@ Bonus points if you could solve it both recursively and iteratively.
 #define PUSH(N) do { p[n ++] = N; } while (0)
 ​
 bool isSymmetric(struct TreeNode* root) {
-    struct TreeNode **p, **tmp;
-    int dep, n, i, j, k;
-    
-    if (!root) return true;
-    
-    dep = 0; n = 0;
-    p = malloc((1 << dep) * sizeof(struct TreeNode *));
-    //assert(p);
-    PUSH(root);
-    while (n) {
-        for (i = 0, j = n - 1; i < j; i ++, j --) {
-            if ((!p[i] && p[j]) ||
-                (!p[j] && p[i]) ||
-                ( p[i] && p[i]->val != p[j]->val)) {
-                free(p);
-                return false;
-            }
-        }
-        tmp = p; k = n;
-        dep ++; n = 0;
-        p = malloc((1 << dep) * sizeof(struct TreeNode *));
-        //assert(p);
-        for (i = 0; i < k; i ++) {
-            if (tmp[i]) {
-                PUSH(tmp[i]->left);
-                PUSH(tmp[i]->right);
-            }
-        }
-        free(tmp);
-    }
-    free(p);
-    return true;
-}
+    struct TreeNode **p, **tmp;
+    int dep, n, i, j, k;
 
+    if (!root) return true;
+
+    dep = 0; n = 0;
+    p = malloc((1 << dep) * sizeof(struct TreeNode *));
+
+    PUSH(root);
+    //printf("root ["); for(int z=0; z<n; z++) {if(p[z]) printf("[%d,%d]", z, p[z]->val);} printf("] n=%d\n", n);
+    while (n) {
+        for (i = 0, j = n - 1; i < j; i ++, j --) {
+            //printf("     i=%d, j=%d, ", i, j);
+            if ((!p[i] && p[j]) ||
+                (!p[j] && p[i]) ||
+                ( p[i] && p[i]->val != p[j]->val)) {
+                //printf("->different, free0()\n");
+                free(p);
+                return false;
+            } //else printf("->same\n");
+        }
+        //printf("1    ["); for(int z=0; z<n; z++) {if(p[z]) printf("[%d,%d]", z, p[z]->val);} printf("] n=%d\n", n);
+
+        tmp = p; k = n;
+        dep ++; n = 0;
+        p = malloc((1 << dep) * sizeof(struct TreeNode *));
+
+        for (i = 0; i < k; i ++) {
+            if (tmp[i]) {
+                PUSH(tmp[i]->left);
+                PUSH(tmp[i]->right);
+
+                //printf("  (%d)[", i); for(int z=0; z<n; z++) {if(p[z]) printf("[%d,%d]", z, p[z]->val, n);} printf("] n=%d\n", n);
+            }
+        }
+        //if(tmp[0]) printf("     free1(%d)\n\n", tmp[0]->val);
+        //else printf("     free1()\n\n");
+        free(tmp);
+        //printf("2    ["); for(int z=0; z<n; z++) {if(p[z]) printf("[%d,%d] ", z, p[z]->val);} printf("] n=%d\n", n);
+    }
+    free(p);
+    return true;
+}
 
 /*
 Difficulty:Easy
