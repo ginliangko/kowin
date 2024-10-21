1b4fe007c47ac1f158e7c89b1e0d3166b63e4df6
Gl Ko <gl_ko@hotmail.com>
2024-10-21 21:14:07 +0800

    Update 127. Word Ladder.c

--- CodeTraining/Leetcode/C/127. Word Ladder.c
+++ CodeTraining/Leetcode/C/127. Word Ladder.c
@@ -1,43 +1,75 @@
 /*
 127. Word Ladder
-
 Given two words (beginWord and endWord), and a dictionary's word list, find the length of shortest transformation sequence from beginWord to endWord, such that:
-
 
 Only one letter can be changed at a time.
 Each transformed word must exist in the word list. Note that beginWord is not a transformed word.
 
-
-
 For example,
-
-
 Given:
 beginWord = "hit"
 endWord = "cog"
 wordList = ["hot","dot","dog","lot","log","cog"]
 
-
 As one shortest transformation is "hit" -> "hot" -> "dot" -> "dog" -> "cog",
 return its length 5.
 
-
-
 Note:
-
 Return 0 if there is no such transformation sequence.
 All words have the same length.
 All words contain only lowercase alphabetic characters.
 You may assume no duplicates in the word list.
 You may assume beginWord and endWord are non-empty and are not the same.
 
-
-
-
 UPDATE (2017/1/20):
 The wordList parameter had been changed to a list of strings (instead of a set of strings). Please reload the code definition to get the latest changes.
 */
 
+// Runtime 228ms Beats 80.00%, Memory 9.80MB Beats 90.00%
+bool isOneLetterDiff(char *a, char *b) {
+    int diffCnt=0;
+    while(*a) {
+        if(*(a++) != *(b++))    diffCnt++;
+        if(diffCnt>1)   return false;
+    }
+    return true;
+}
+
+int ladderLength(char* beginWord, char* endWord, char** wordList, int wordListSize) {
+    if(!beginWord || !endWord || !wordList || wordListSize==0)  return 0;
+
+    bool *visited=calloc(wordListSize, sizeof(bool));
+    char **q=malloc((wordListSize+1)*sizeof(char *));
+    int s=0, e=0, d=0;
+
+    q[e++]=beginWord;
+    d++;
+    while(s<e) {
+        int size=e-s;
+        for(int i=0; i<size; i++) {
+            char *word=q[s++];
+            if(strcmp(word, endWord)==0) {
+                free(visited);
+                free(q);
+                return d;
+            }
+
+            for(int j=0; j<wordListSize; j++) {
+                if(!visited[j] && isOneLetterDiff(word, wordList[j])) {
+                    visited[j]=true;
+                    q[e++]=wordList[j];
+                }
+            }
+        }
+        d++;
+    }
+
+    free(visited);
+    free(q);
+    return 0;
+}
+
+/* Original solution
 typedef struct {
     char **q;
     int n;
@@ -97,6 +129,7 @@ int ladderLength(char* beginWord, char* endWord, char** wordList, int wordListSi
     
     return n + 1;
 }
+*/
 
 /*
 Difficulty:Medium
