1348cb74e692dd77a65d28d00a310ce4d79f6f08
Gl Ko <gl_ko@hotmail.com>
2024-10-24 13:44:15 +0800

    Update 48. Rotate Image.c

--- CodeTraining/Leetcode/C/48. Rotate Image.c
+++ CodeTraining/Leetcode/C/48. Rotate Image.c
@@ -5,6 +5,14 @@ You are given an n x n 2D matrix representing an image.
 Rotate the image by 90 degrees (clockwise).
 Follow up:
 Could you do this in-place?
+
+Example 1:
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [[7,4,1],[8,5,2],[9,6,3]]
+
+Example 2:
+Input: matrix = [[5,1,9,11],[2,4,8,10],[13,3,6,7],[15,14,12,16]]
+Output: [[15,13,2,5],[14,3,4,1],[12,6,8,9],[16,7,10,11]]
 */
 
 void rotate(int** matrix, int matrixSize, int* matrixColSize) {
