f517e56732906b82c204e4c0e8ee01db7b8a0b4a
Gl Ko <gl_ko@hotmail.com>
2024-10-24 15:51:07 +0800

    Update 73. Set Matrix Zeroes.c

--- CodeTraining/Leetcode/C/73. Set Matrix Zeroes.c
+++ CodeTraining/Leetcode/C/73. Set Matrix Zeroes.c
@@ -10,6 +10,7 @@ Example 2:
 Input: matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]]
 Output: [[0,0,0,0],[0,4,5,0],[0,3,1,0]]
 */
+
 void setZeroes(int** matrix, int matrixSize, int* matrixColSize) {
     int n = matrixSize;
     int m = (*matrixColSize);
