b55cd80dd0ca8ec1b727f5b1b4a0b5503480db14
Gl Ko <gl_ko@hotmail.com>
2024-10-25 19:52:53 +0800

    Create 49. Group Anagrams.cpp

---
+++ CodeTraining/Leetcode/C++/49. Group Anagrams.cpp
@@ -0,0 +1,40 @@
+/* 49. Group Anagrams
+Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+
+Example 1:
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+
+Explanation:
+There is no string in strs that can be rearranged to form "bat".
+The strings "nat" and "tan" are anagrams as they can be rearranged to form each other.
+The strings "ate", "eat", and "tea" are anagrams as they can be rearranged to form each other.
+
+Example 2:
+Input: strs = [""]
+Output: [[""]]
+
+Example 3:
+Input: strs = ["a"]
+Output: [["a"]]
+*/
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
+        vector<vector<string>> ans;
+
+        for(auto x:strs) {
+            string word=x;
+            sort(word.begin(), word.end());
+            mp[word].push_back(x);
+        }
+
+        for(auto x:mp) {
+            ans.push_back(x.second);
+        }
+
+        return ans;
+    }
+};
