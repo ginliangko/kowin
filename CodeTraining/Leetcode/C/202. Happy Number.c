b745bfe315da24a6462eeca398e05b50cc8ade58
Gl Ko <gl_ko@hotmail.com>
2024-10-27 00:17:28 +0800

    Update 202. Happy Number.c

--- CodeTraining/Leetcode/C/202. Happy Number.c
+++ CodeTraining/Leetcode/C/202. Happy Number.c
@@ -1,22 +1,38 @@
-/*
-202. Happy Number
-
+/* 202. Happy Number
 Write an algorithm to determine if a number is "happy".
-
 A happy number is a number defined by the following process: Starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.
 
 Example: 19 is a happy number
-
-
 12 + 92 = 82
 82 + 22 = 68
 62 + 82 = 100
 12 + 02 + 02 = 1
 
-
 Credits:Special thanks to @mithmatt and @ts for adding this problem and creating all test cases.
 */
 
+bool isHappy(int n) {
+    int arr[128]={0};
+    int idx=0;
+
+    while(n) {
+        int sum=0;
+        for(int i=0; i<idx; i++)    if(arr[i]==n)   return false;
+
+        arr[idx++]=n;
+        while(n) {
+            int d=n%10;
+            sum+=(d*d);
+            n/=10;
+        }
+        if(sum==1)  return true;
+        n=sum;
+    }
+
+    return false;
+}
+
+/* Original solution
 bool has_cycle(int *set, int x, int k) {
     int i;
     for (i = 0; i < x; i ++) {
@@ -54,7 +70,7 @@ bool isHappy(int n) {
     
     return k == 1 ? true : false;
 }
-
+*/
 
 /*
 Difficulty:Easy
