dca078ecc9d1f1063a23502b5021177b539555d6
Gl Ko <gl_ko@hotmail.com>
2024-10-27 10:41:21 +0800

    Update 219. Contains Duplicate II.c

--- CodeTraining/Leetcode/C/219. Contains Duplicate II.c
+++ CodeTraining/Leetcode/C/219. Contains Duplicate II.c
@@ -5,38 +5,38 @@ Given an array of integers and an integer k, find out whether there are two dist
 */
 
 typedef struct {
-    int val;
-    int idx;
+    int val;
+    int idx;
 } e_t;
-int cmp(void const *a, void const *b) {
-    return ((const e_t *)a)->val < ((const e_t *)b)->val ? -1 :
-           ((const e_t *)a)->val > ((const e_t *)b)->val ?  1 :
-           ((const e_t *)a)->idx < ((const e_t *)b)->idx ? -1 : 1;
-}
-bool containsNearbyDuplicate(int* nums, int numsSize, int k) {
-    e_t *p;
-    int i, f;
-    
-    p = malloc(numsSize * sizeof(e_t));
-    //assert(p);
-    for (i = 0; i < numsSize; i ++) {
-        p[i].val = nums[i];
-        p[i].idx = i;
-    }
-    
-    qsort(p, numsSize, sizeof(e_t), cmp);
-    
-    f = 0;
-    for (i = 1; !f && i < numsSize; i ++) {
-        if (p[i].val == p[i - 1].val &&
-            p[i].idx - p[i - 1].idx <= k) f = 1;
-    }
-    
-    free(p);
-    
-    return f;
+
+int cmpFunc(const void *a, const void *b) {
+    return ((e_t *)a)->val -((e_t *)b)->val;
 }
 
+bool containsNearbyDuplicate(int* nums, int numsSize, int k) {
+    e_t *p;
+    int i, ret=0;
+
+    p = malloc(numsSize * sizeof(e_t));
+
+    for (i = 0; i < numsSize; i ++) {
+        p[i].val = nums[i];
+        p[i].idx = i;
+    }
+
+    qsort(p, numsSize, sizeof(e_t), cmpFunc);
+
+    for (i = 1; !f && i < numsSize; i ++) {
+        if (p[i].val == p[i-1].val && p[i].idx - p[i-1].idx <= k) {
+             ret = 1;
+             break;
+        }
+    }
+
+    free(p);
+
+    return ret;
+}
 
 /*
 Difficulty:Easy
